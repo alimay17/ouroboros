abf868a8971018ac08063279f79476a7a572836d
Alice Smith <alimay.smith@outlook.com>
2025-02-09 13:19:01 -0700

    test

--- src/index.js
+++ src/index.js
@@ -24,5 +24,5 @@ app.post("/end", (req, res) => {
 
 // Start the Express server
 app.listen(port, () => {
-    console.log(`Starting Battlesnake Server at http://0.0.0.0:${port}...`)
+    console.log(`Starting BattleSnake Server at http://0.0.0.0:${port}...`)
 })
